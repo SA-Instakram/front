eb426617894c259cc8eb9f1004af99d932d2faa9
jihunchoi <geehoon62017@gmail.com>
2023-11-06 20:45:31 +0900

    feat. profile page 구현

---
+++ src/pages/Profile/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 47.5px;
+  width: 100%;
+  height: 100%;
+`;
