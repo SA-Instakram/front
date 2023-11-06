9a341ac8bdf760067eb083f8676a6219bf92507b
Seho-P <qkrtpgh77@gmail.com>
2023-11-06 20:14:22 +0900

    prettier

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,39 +1,47 @@
 import React, { useState } from 'react';
-import { useNavigate } from "react-router-dom";
-import { Wrapper,loginbuttonStyle,headingstyle } from "./styles";
-import './styles.css'
-
+import { useNavigate } from 'react-router-dom';
+import { Wrapper, loginbuttonStyle, headingstyle, CustomInput, BoldText } from './styles';
 export default function Loginpage() {
-    const navigate = useNavigate();
-    let [Email, setEmail] = useState("");
-    let [Password, setPassword] = useState("");
-    const onEmailHandler = (event) => {
-        setEmail(event.currentTarget.value);
-    }
-    const onPasswordHandler = (event) => {
-        setPassword(event.currentTarget.value);
-    }
-    
-    return (
-        <Wrapper>
-            <form style={{ display: 'flex', flexDirection: 'column'}}
-            >
-                <h1 style={headingstyle}>Instakram</h1>
-                <label>Email</label>
-                <input placeholder="전화번호 사용자 이름 또는 이메일" type='email' className='customInput' value={Email} onChange={onEmailHandler}/>
-                <label>Password</label>
-                <input placeholder="비밀번호" type='password' className='customInput' value={Password} onChange={onPasswordHandler}/>
-                <br />
-                <button style={loginbuttonStyle} 
-                    onClick={() => {
-                    navigate("/");
-                }}>
-                    <p className="bold-text">
-                    로그인
-                    </p>
-                </button>
-            </form>
-        </Wrapper>
-    )
-}
+  const navigate = useNavigate();
+  let [Email, setEmail] = useState('');
+  let [Password, setPassword] = useState('');
+  const onEmailHandler = (event) => {
+    setEmail(event.currentTarget.value);
+  };
+  const onPasswordHandler = (event) => {
+    setPassword(event.currentTarget.value);
+  };
 
+  return (
+    <Wrapper>
+      <form style={{ display: 'flex', flexDirection: 'column' }}>
+        <h1 style={headingstyle}>Instakram</h1>
+        <label>Email</label>
+        <CustomInput
+          placeholder="전화번호 사용자 이름 또는 이메일"
+          type="email"
+          className="customInput"
+          value={Email}
+          onChange={onEmailHandler}
+        />
+        <label>Password</label>
+        <CustomInput
+          placeholder="비밀번호"
+          type="password"
+          className="customInput"
+          value={Password}
+          onChange={onPasswordHandler}
+        />
+        <br />
+        <button
+          style={loginbuttonStyle}
+          onClick={() => {
+            navigate('/');
+          }}
+        >
+          <BoldText>로그인</BoldText>
+        </button>
+      </form>
+    </Wrapper>
+  );
+}
--- src/pages/login/styles.js
+++ src/pages/login/styles.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   display: flex;
@@ -6,18 +6,28 @@ export const Wrapper = styled.div`
   align-items: center;
   width: 100%;
   height: 100vh;
-
-  
-
 `;
 export const headingstyle = {
-    padding : '0px 60px 0px 60px'
-}
+  padding: '0px 60px 0px 60px',
+};
 
 export const loginbuttonStyle = {
-    backgroundColor: '#77A7FF',
-    color: 'white',
-    border : '1px solid #ccc',
-    borderRadius : '10px'
-  };
+  backgroundColor: '#77A7FF',
+  color: 'white',
+  border: '1px solid #ccc',
+  borderRadius: '10px',
+};
 
+export const CustomInput = styled.input`
+  background-color: rgb(231, 225, 225);
+  padding: 10px;
+  margin: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+`;
+
+export const BoldText = styled.p`
+  font-size: 15px;
+  font-weight: bold;
+  margin: 7px;
+`;
