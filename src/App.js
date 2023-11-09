6bd262e40bf66f3a2e98525fb7f95baad8746785
Seho-P <qkrtpgh77@gmail.com>
2023-11-09 13:19:46 +0900

    conflict

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./App.css";
 import Main from "./pages/Main/main";
 import Profile from "./pages/Profile/profile";
 import Loginpage from "./pages/login/login";
-import Registerpage from "./pages/Register/register";
 import NewFeed from "./pages/NewFeed/newFeed";
+import Registerpage from "./pages/Register/register";
 
 function App() {
   return (
@@ -13,8 +13,8 @@ function App() {
         <Route path="/" element={<Main />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/login" element={<Loginpage />} />
-        <Route path="/register" element={<Registerpage />} />
         <Route path="/newfeed" element={<NewFeed />} />
+        <Route path="/register" element={<Registerpage />} />
       </Routes>
     </div>
   );
